f42846e2ae10a8dc5bf451c9a68185416425fc02
Justin Young <whyjustin@yahoo.com>
2018-05-31 14:31:14 -0700

    Remove siesta from REST endpoints. NEXUS-14940.

--- nexus.sh
+++ nexus.sh
@@ -18,5 +18,5 @@ until curl --fail --insecure https://localhost; do
   sleep 1
 done
 
-curl -v -u admin:admin123 --insecure --header 'Content-Type: application/json' 'https://localhost/service/siesta/rest/v1/script' -d @nexus-repository/create-docker-proxy.json
-curl -v -X POST -u admin:admin123 --insecure --header 'Content-Type: text/plain' 'https://localhost/service/siesta/rest/v1/script/CreateDockerProxy/run'
+curl -v -u admin:admin123 --insecure --header 'Content-Type: application/json' 'https://localhost/service/rest/v1/script' -d @nexus-repository/create-docker-proxy.json
+curl -v -X POST -u admin:admin123 --insecure --header 'Content-Type: text/plain' 'https://localhost/service/rest/v1/script/CreateDockerProxy/run'
